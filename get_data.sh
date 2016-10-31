f55e1e4f5bd62a9893b4a32c0373749b0bf6becd
Igor Shalyminov <ishalyminov@gmail.com>
2016-10-31 00:18:17 +0000

    working tools added

---
+++ get_data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget http://opus.lingfil.uu.se/download.php?f=OpenSubtitles/en.tar.gz -O en.tar.gz
+tar -xvzf en.tar.gz
+rm en.tar.gz
+
+for file in `find OpenSubtitles -name *.gz`; do
+  gzip -d $file
+done
